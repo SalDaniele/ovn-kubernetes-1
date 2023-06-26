573cc7b71b30b1fa89a8e7208cdaa5db6a95f771
Salvatore Daniele <sdaniele@redhat.com>
2023-06-26 17:02:39 -0400

    Use NodePrimaryIp to set host-addresses in DPU mode
    When running a 2-cluster design in DPU mode, currently host-addresses
    is set to the IP address of the Infrastructure node (i.e. Bluefield
    node) running of behalf of the tenant cluster. This will cause all
    NodePort service to fail in DPU mode.
    
    Instead, we should calculate the host-addresses using the NodePrimaryIP
    which will set the host-address to the Node IP of the tenant node.
    
    Signed-off-by: Salvatore Daniele <sdaniele@redhat.com>

--- go-controller/pkg/node/node_ip_handler_linux.go
+++ go-controller/pkg/node/node_ip_handler_linux.go
@@ -9,11 +9,14 @@ import (
 	"sync"
 	"time"
 
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/kube"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 
 	"github.com/vishvananda/netlink"
+	kapi "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
@@ -234,7 +237,7 @@ func (c *addressManager) updateNodeAddressAnnotations() error {
 	}
 
 	// update k8s.ovn.org/host-addresses
-	if err = util.SetNodeHostAddresses(c.nodeAnnotator, c.addresses); err != nil {
+	if c.updateHostAddresses(node) != nil {
 		return err
 	}
 
@@ -262,6 +265,21 @@ func (c *addressManager) updateNodeAddressAnnotations() error {
 		return err
 	}
 	return nil
+}
+
+func (c *addressManager) updateHostAddresses(node *kapi.Node) error {
+	if config.OvnKubeNode.Mode == types.NodeModeDPU {
+		// For DPU mode, here we need to use the DPU host's IP address which is the tenant cluster's
+		// host internal IP address instead.
+		nodeAddrStr, err := util.GetNodePrimaryIP(node)
+		if err != nil {
+			return err
+		}
+		nodeAddrSet := sets.New[string](nodeAddrStr)
+		return util.SetNodeHostAddresses(c.nodeAnnotator, nodeAddrSet)
+	}
+
+	return util.SetNodeHostAddresses(c.nodeAnnotator, c.addresses)
 }
 
 func (c *addressManager) assignAddresses(nodeHostAddresses sets.Set[string]) bool {
